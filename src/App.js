00d33b03845880f00d1e805bfded3f622502c322
jurisportfolio <jurko.bossetroll@gmail.com>
2019-07-11 21:18:31 +0200

    get state from ListContainer to App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import jsonp from 'jsonp';
 import './App.css';
 import FormComponent from './components/FormComponent';
 import ListContainer from './components/ListContainer';
 
 
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="App">
-      <FormComponent />
-      <ListContainer />
-    </div>
-  );
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      roomsData: null,
+      error: null,
+      isLoading: false
+    };
+  }
+
+  handleClick() {
+    this.setState({ roomsData: null, isLoading: true });
+    getListOfRooms(
+      (error, roomsData) => this.setState({ roomsData, error, isLoading: false })
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FormComponent />
+        <button onClick={this.handleClick}>get rooms</button>
+        <ListContainer props={this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+function getListOfRooms(callback) {
+  let url = `http://testapi.itur.pl/api.php?date_from=2019-08-01&date_to=2019-08-06&nb_adults=2&nb_children=1`;
+  jsonp(
+    url,
+    (error, data) => callback(error, data)
+  );
+}
--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,45 +1,20 @@
 import React from 'react';
-import jsonp from 'jsonp';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import RoomsListComponent from './RoomsListComponent'
 
-export default class ListContainer extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      roomsData: null,
-      error: null,
-      isLoading: false
-    };
-  }
-
-  handleClick() {
-    this.setState({ roomsData: null, isLoading: true });
-    getListOfRooms(
-      (error, roomsData) => this.setState({ roomsData, error, isLoading: false })
-    );
-  }
-
-  render() {
-    return (
+const ListContainer = ({ props }) => {
+  const { isLoading, error, roomsData } = props;
+  return (
+    <div>
       <div>
-        <button onClick={this.handleClick}>get rooms</button>
-        <div>
-          {this.state.isLoading ? <CircularProgress /> : null}
-          {this.state.error ? <h2>{this.state.error}</h2> : null}
-          {this.state.roomsData ? <RoomsListComponent roomsData={this.state.roomsData} /> : null}
-        </div>
+        {isLoading ? <CircularProgress /> : null}
+        {error ? <h2>{error}</h2> : null}
+        {roomsData ? <RoomsListComponent roomsData={roomsData} /> : null}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-function getListOfRooms(callback) {
-  let url = `http://testapi.itur.pl/api.php?date_from=2019-08-01&date_to=2019-08-06&nb_adults=2&nb_children=1`;
-  jsonp(
-    url,
-    (error, data) => callback(error, data)
-  );
-}
+export default ListContainer;
+
+
