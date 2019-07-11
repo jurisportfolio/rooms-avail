7496ee751a3bb3bd8d86bafcc8817c04a61f31b1
jurisportfolio <jurko.bossetroll@gmail.com>
2019-07-12 01:48:37 +0200

    Updates

---
+++ precache-manifest.1200a8e0fc3a5ea3212d7921e449bd55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f253b934cb12e306cf11aa595d045a",
+    "url": "/rooms-avail/index.html"
+  },
+  {
+    "revision": "a203813d5a949ead60fb",
+    "url": "/rooms-avail/static/css/main.ba7c25f3.chunk.css"
+  },
+  {
+    "revision": "b7c8d73c6a89edfbecef",
+    "url": "/rooms-avail/static/js/2.aac57b9e.chunk.js"
+  },
+  {
+    "revision": "a203813d5a949ead60fb",
+    "url": "/rooms-avail/static/js/main.f496c11f.chunk.js"
+  },
+  {
+    "revision": "b7e185b7a572332118a1",
+    "url": "/rooms-avail/static/js/runtime~main.3e233093.js"
+  }
+]);
